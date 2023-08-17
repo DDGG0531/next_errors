38cfb7283c3066b84ac14aceebec79985dfe2e27
JimDong <jimdong@damaiapp.com.tw>
2023-08-17 17:37:04 +0800

    use be message to customize e11 error

--- src/pages/e11.tsx
+++ src/pages/e11.tsx
@@ -1,11 +1,20 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import { Button } from "@/components/ui/button";
+import * as Sentry from "@sentry/nextjs";
 
 export default function E10() {
   async function handleClick() {
-    const res = await axios.post("/api/e11", {
-      name: "Jim",
-    });
+    try {
+      const res = await axios.post("/api/e11", {
+        name: "Jim",
+      });
+    } catch (error) {
+      if (isAxiosError(error)) {
+        let message = error.response!.data.message;
+        console.log("message", message);
+        Sentry.captureException(new Error(message));
+      }
+    }
   }
 
   return <Button onClick={handleClick}>Button</Button>;
