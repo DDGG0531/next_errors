d2aae4d7902635f63a02f8a751deb91b7e8f9494
JimDong <jimdong@damaiapp.com.tw>
2023-08-22 11:08:15 +0800

    add ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3N7RHFYQ4G"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3N7RHFYQ4G');
+      </script>
       <body>
         <Main />
         <NextScript />
