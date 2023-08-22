8c744edc204426924d56eb7d8c6d19ea2bab4e70
JimDong <jimdong@damaiapp.com.tw>
2023-08-22 11:10:35 +0800

    use Script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,18 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3N7RHFYQ4G"></script>
-      <script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3N7RHFYQ4G"></Script>
+      <Script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'G-3N7RHFYQ4G');
-      </script>
+      </Script>
       <body>
         <Main />
         <NextScript />
